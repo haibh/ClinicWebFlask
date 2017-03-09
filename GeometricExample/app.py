95813dc78a24173498f5df8a3872bc743440b91a
haibh <haibhut@gmail.com>
2017-03-09 18:12:28 +0700

    GeoTest

--- GeometricExample/app.py
+++ GeometricExample/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__)
 
 
@@ -9,6 +8,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form['email_name']
+        height = request.form['height_name']
+        print email, height
+    return render_template("success.html")
+
+
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(port=8000)
