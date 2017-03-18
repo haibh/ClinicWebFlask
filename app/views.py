127e1572f6d2e12eed9561983af316a1edf55f4b
haibhut <haibhut@gmail.com>
2017-03-18 23:49:41 +0700

    MegaCourse

--- app/views.py
+++ app/views.py
@@ -10,13 +10,15 @@ def index():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!  <a href='/logout'>Logout</a>"
+        # return "Hello Boss!  <a href='/logout'>Logout</a>"
+        return render_template('index.html', title='Clinic Management', user=session['username'])
 
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+    if request.form['password'] == '1' and request.form['username'] == '1':
         session['logged_in'] = True
+        session['username'] = request.form['username']
     else:
         flash('wrong password!')
     return index()
