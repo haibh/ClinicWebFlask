b4478bbc3a2cbf98a0a9feff6d71458a971b9913
haibhut <haibhut@gmail.com>
2017-03-18 11:35:07 +0700

    MegaCourse

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
+from app import app
+
+
+@app.route('/')
+def index():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!  <a href='/logout'>Logout</a>"
+
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
