7eea320b6d445c7880b7c4e667a73893250f9393
haibh <haibhut@gmail.com>
2017-03-20 14:44:43 +0700

    Clinic Starting

--- app/models.py
+++ app/models.py
@@ -1,28 +1,84 @@
-from sqlalchemy import *
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+# from sqlalchemy import *
+# from sqlalchemy import create_engine, ForeignKey
+# from sqlalchemy import Column, Date, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship, backref
+#
+# engine = create_engine('sqlite:///tutorial.db', echo=True)
+# Base = declarative_base()
+#
+#
+# ########################################################################
+# class User(Base):
+#     """"""
+#     __tablename__ = "users"
+#
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String)
+#     password = Column(String)
+#
+#     # ----------------------------------------------------------------------
+#     def __init__(self, username, password):
+#         """"""
+#         self.username = username
+#         self.password = password
+#
+#
+# # create tables
+# Base.metadata.create_all(engine)
 
-engine = create_engine('sqlite:///tutorial.db', echo=True)
-Base = declarative_base()
+from app import db
 
 
-########################################################################
-class User(Base):
-    """"""
-    __tablename__ = "users"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True, unique=True)
+    password = db.Column(db.String(32), index=True, unique=True)
+    email = db.Column(db.String(32), index=True, unique=True)
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
-
-    # ----------------------------------------------------------------------
-    def __init__(self, username, password):
-        """"""
-        self.username = username
-        self.password = password
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 
-# create tables
-Base.metadata.create_all(engine)
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_name = db.Column(db.String(32), index=True, unique=True)
+    patient_phone = db.Column(db.String(32), index=True, unique=True)
+    patient_age = db.Column(db.String(32), index=True, unique=True)
+    patient_birth_year = db.Column(db.String(32), index=True, unique=True)
+    patient_history = db.Column(db.String(32), index=True, unique=True)
+    patient_family_history = db.Column(db.String(32), index=True, unique=True)
+    patient_description = db.Column(db.String(32), index=True, unique=True)
+    patient_diagnostic = db.Column(db.String(32), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.patient_name)
+
+
+class Medicine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    medicine_name = db.Column(db.String(32), index=True, unique=True)
+    medicine_code = db.Column(db.String(32), index=True, unique=True)
+    medicine_group = db.Column(db.String(32), index=True, unique=True)
+    medicine_active_element = db.Column(db.String(32), index=True, unique=True)
+    medicine_unit = db.Column(db.String(32), index=True, unique=True)
+    medicine_inventory = db.Column(db.String(32), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.medicine_name)
+
+
+class Diagnostic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    diagnostic_name = db.Column(db.String(32), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.diagnostic_name)
+
+
+class Treatment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    treatment_name = db.Column(db.String(32), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.treatment_name)
