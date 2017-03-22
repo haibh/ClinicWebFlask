644e05b22e694d171f8d9a905cd3a79746e4cff1
haibh <haibhut@gmail.com>
2017-03-22 13:37:34 +0700

    Database

--- app/static/custom.js
+++ app/static/custom.js
@@ -2,9 +2,19 @@
  * Created by haibh on 22-Mar-17.
  */
 // Add your javascript here
+// $(function () {
+//     $(".nav li").on("click", function () {
+//         $(".nav li").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });
+
 $(function () {
-    $(".nav li").on("click", function () {
-        $(".nav li").removeClass("active");
-        $(this).addClass("active");
-    });
+    $('.nav li').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
 });
+
+
+
+
